4dd2a0e842acf93ff0506ad3b0b8722c806b92b8
huyhoangm2 <hoangken760@gmail.com>
2023-01-07 00:53:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function hex2bin(hex){
+    return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-8);
+}
+
+    console.log(hex2bin('FF'))
+function hex2bin(hex){
+     return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-8);
+}
+    
+     console.log(hex2bin('AA'))
+
+ function hex2bin(hex){
+     return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-8);
+ }
+        
+     console.log(hex2bin('FA'))
+            
