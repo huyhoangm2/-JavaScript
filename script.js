29cc91fb0de5bbf00b2f23c363750dd6f305167c
huyhoangm2 <hoangken760@gmail.com>
2023-01-10 16:12:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+var arr1 = [1, 2, 3, 4]
+var minarr1 = Math.min.apply(Math, arr1)
+var maxarr1 = Math.max.apply(Math, arr1)
+console.log(minarr1)
+console.log(maxarr1)
+var arr2 = [0, 10]
+var minarr2 = Math.min.apply(Math, arr2)
+var maxarr2 = Math.max.apply(Math, arr2)
+console.log(minarr2)
+console.log(maxarr2)
+
+if (minarr1 > minarr2, maxarr1 < maxarr2) {
+    console.log("true")
+    
+} else {
+    console.log("false")
+    
+}
+
+var arr3 = [1, 2, 3]
+var minarr3 = Math.min.apply(Math, arr3)
+var maxarr3 = Math.max.apply(Math, arr3)
+console.log(minarr3)
+console.log(maxarr3)
+var arr4 = [2, 3]
+var minarr4 = Math.min.apply(Math, arr4)
+var maxarr4 = Math.max.apply(Math, arr4)
+console.log(minarr4)
+console.log(maxarr4)
+
+if (minarr3 > minarr4, maxarr3 < maxarr4) {
+    console.log("true")    
+} else {
+    console.log("false")
+    
+}
+
